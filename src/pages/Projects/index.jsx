7ac61a504fe573e6780ea96bc428d2e6cf5e3667
Lucas Aguero <Lucas-Aguero@ajvierci.com.py>
2023-08-03 13:17:32 -0400

    work 29

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -8,17 +8,15 @@ export const  ProjectPage  = () => {
     <div id='projects' className="border-green-300 flex-col justify-center items-center flex w-full h-full">
         <h1> TITLE </h1>  
 
-        <section className='border-2 border-rose-600 w-5/6 h-auto grid grid-cols-2 gap-8 '>
+        <section className='border-2 border-rose-600 w-5/6 grid grid-cols-2 gap-8 '>
           {projects.map(({projectLink, projectImage,projectRepo, projectName, projectDescription, projectSkill, id})=>(
-            <div key={id} className='gap-4 border-2 h-auto border-teal-500'>
+            <div key={id} className='gap-4 flex flex-col border-2  border-teal-500'>
               
-              <section className='border-2 border-pink-400'>
-                <img src={projectImage} alt='testing image'></img>
-              </section>
-
-              <div className={`${flexBetweeen} flex-col`}>
-              
-                <section>
+              <div className={`flex flex-col`}>
+                
+                <img className='border-2 w-full border-pink-400' src={projectImage} alt='testing image'></img>
+    
+                <section className={`${flexBetweeen} flex-col`}>
                   <h2 className='border-2 border-blue-300 '>{projectName}</h2>
                   <p className=''>{projectDescription}</p>
                 </section>
@@ -35,19 +33,20 @@ export const  ProjectPage  = () => {
                 </section>
 
               </div>
-              
+
               <section className='border-2 flex flex-row gap-2 p-2 border-b-red-950'>
-                <a 
-                href={projectLink} 
-                className={` ${flexBetweeen} w-20 flex-row hover:bg-red-400 bg-orange-500 p-1 rounded-sm gap-1`}>
-                  Live <SiLinktree/>
-                </a>
-                <a 
-                href={projectRepo}
-                className={`${flexBetweeen} w-20 flex-row hover:bg-red-400 bg-orange-500 p-1 rounded-sm gap-1`}>
-                  Source <SiGithub/>
-                </a>
+                  <a 
+                  href={projectLink} 
+                  className={` ${flexBetweeen} w-20 flex-row hover:bg-red-400 bg-orange-500 p-1 rounded-sm gap-1`}>
+                    Live <SiLinktree/>
+                  </a>
+                  <a 
+                  href={projectRepo}
+                  className={`${flexBetweeen} w-20 flex-row hover:bg-red-400 bg-orange-500 p-1 rounded-sm gap-1`}>
+                    Source <SiGithub/>
+                  </a>
               </section>
+
             </div>
           ))}
         </section>
